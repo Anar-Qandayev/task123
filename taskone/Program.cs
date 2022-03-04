84cb1bd24619c1c0437b9ed8e99e55989749aae7
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-03-04 23:44:42 +0400

    first commit

---
+++ taskone/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            if (n >= m)
+            {
+                Console.Write("m ededi n ededinden boyuk olmalidir");
+            }
+            int count = 0;
+
+            for (int i = n; i <= m; i++)
+            {
+                if (i % x == 0) count++;
+            }
+            Console.WriteLine(n + " Ededi ile " + m + " ededi arasında " + x + " ededine bolunen ededlerin sayı: " + count);
+            else
+            {
+                Console.WriteLine("Sistemde xeta bas verdi!!!");
+            }
+
+        }
+
+    }
+}
